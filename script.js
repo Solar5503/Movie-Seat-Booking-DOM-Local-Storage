f267d8afea6262c3b3b22cc6949ca350ea30c117
Solar5503 <spidervvv@mail.ru>
2021-09-24 14:28:04 +0300

    Populate UI with saved data

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
+populateUI();
 
 //save selected movie and price
 function setMovieData(movieIndex, moviePrice) {
@@ -14,14 +15,34 @@ function setMovieData(movieIndex, moviePrice) {
 //update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  const selectedSeatsIndex = [...selectedSeats].map((seat) =>
+    [...seats].indexOf(seat)
+  );
+  localStorage.setItem('selectedSeats', JSON.stringify(selectedSeatsIndex));
   const selectedSeatsCount = selectedSeats.length;
   count.textContent = selectedSeatsCount;
   total.textContent = selectedSeatsCount * ticketPrice;
 }
 
+//Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  const selectedMoviePrice = localStorage.getItem('selectedMoviePrice');
+  if (selectedMoviePrice !== null) {
+    ticketPrice = selectedMoviePrice;
+  }
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
@@ -39,3 +60,6 @@ container.addEventListener('click', (e) => {
   }
   updateSelectedCount();
 });
+
+//Initial count and total set
+updateSelectedCount();
